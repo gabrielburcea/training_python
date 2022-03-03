8c5063434d0518f3faab32def19b5bb64b9ee93b
gabrielburcea <gb.burcea@gmail.com>
2022-03-03 14:55:29 +0000

    I have added new lines of code; section 5

--- strings.py
+++ strings.py
@@ -5,13 +5,19 @@ print('We can even include "quotes" in strings')
 print("hello" + " world")
 
 greeting = "Hello"
-name = input("Please enter your name")
+name = "Tim"
 print(greeting + name) #no space
-#name = "Bruce"
+name = "Bruce"
 #if we want a space, we can add that too
 print(greeting + ' ' + name) #it will give me a space
 
+# other points in 4th section of the course
+age = 24
+print(age)
 
+print(type(age))
+print(type(greeting))
 
-
-
+age = "2 years"
+print(age)
+print(type(age))
