59c9db79b5905e0256e9df0e6f360f01a6fb13ff
gabrielburcea <gb.burcea@gmail.com>
2022-03-03 17:02:58 +0000

    exercising wiith slicing

---
+++ slicing.py
@@ -0,0 +1,23 @@
+#                     1
+#         012345678911234
+parrot = "Norwegian Blue"
+
+print(parrot[0:6]) #Norweg it does not include position 6
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] +parrot[6:])
+
+print(parrot[:])
+
+letters = "abcdefghijklmnoprstuvwxyz"
+
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
