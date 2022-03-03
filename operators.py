62b7dae004e28fa0e234116528f0b4d2dda0595b
gabrielburcea <gb.burcea@gmail.com>
2022-03-03 15:53:25 +0000

    I have added new lines of code; section 5

---
+++ operators.py
@@ -0,0 +1,23 @@
+a = 12
+b = 3
+
+print(a + b)   # 15
+print(a - b)   # 9
+print(a * b)   # 16
+print(a / b)   # 4.0
+print(a // b)  # 4
+print(a % b)    # 0 modulo: the remainder after the integer division
+
+print()
+
+for i in range(1, a // b):
+    print(i)
+
+i = 1
+print(i)
+
+i = 2
+print(i)
+
+i = 3
+print(i)
