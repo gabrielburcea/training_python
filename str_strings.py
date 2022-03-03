fd7f87ec095bf68eb178b37f8c890cf6d505ef1e
gabrielburcea <gb.burcea@gmail.com>
2022-03-03 17:03:40 +0000

    exercising with str

---
+++ str_strings.py
@@ -0,0 +1,35 @@
+#                     1
+#         012345678911234
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+# we can use indexing using negative number
+
+print()
+print(parrot[-1])
+print(parrot[-14])
+
+print()
+# exercise
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
