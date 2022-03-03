89c50684224d0cd469150fd1609c2e3177568635
gabrielburcea <gb.burcea@gmail.com>
2022-03-03 17:43:24 +0000

    negative indexing and idioms

---
+++ slicing_barckwards.py
@@ -0,0 +1,17 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[25:0:-1]
+print(backwards)
+backwards_1 = letters[25::-1]
+print(backwards_1)
+backwards_2 = letters[::-1]
+print(backwards_2)
+
+print(letters[16:13:-1])
+
+print(letters[25:17:-1])
+print(letters[:-9:-1])
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
