95e6b3eee449a317bfb4b7f73e86ad7ba49c68cc
gabrielburcea <gb.burcea@gmail.com>
2022-03-09 11:17:47 +0000

    bolean added in conjuction with and and or

---
+++ truefalse_and_or.py
@@ -0,0 +1,24 @@
+# day = "Friday"
+# temperature = 30
+# raining = True # here I can change to False
+#
+# # Take into account the operator precendence - and takes precendence over or
+#
+# if (day == "Saturday" and temperature > 27) or not raining:
+#     print("Go swimming")
+# else:
+#     print("Learn Python")
+
+if 0:
+    print("True")
+else:
+    print("False")
+
+name = input("Please enter your name: ")
+if name:
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man with no name?")
+
+
+
